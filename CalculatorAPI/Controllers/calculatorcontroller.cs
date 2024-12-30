99d62b217fb8fa6e763a222699841e0ce1df6c6e
Samira <nezhadsamira9@gmail.com>
2024-12-30 02:04:33 -0800

    first commit

---
+++ CalculatorAPI/Controllers/calculatorcontroller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class calculatorcontroller : ControllerBase
+    {
+        public IActionResult Sum(FromQuery] long FirstNumber)
+           
+            throw new NotImplementedException();
+
+
+        public IActionResult Minus(FromQuery] long SecondNumber)
+
+            throw new NotImplementedException();
+    }
+}
+    
+   
+   
+
+
